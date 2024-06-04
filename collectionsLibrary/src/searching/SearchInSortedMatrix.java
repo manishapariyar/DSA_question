bf7463c22159509e75ad5d11c2da7122032e1a2f
manishapariyar <manishapariyar196@gmail.com>
2024-06-04 10:18:01 +0545

    binary search question

---
+++ collectionsLibrary/src/searching/SearchInSortedMatrix.java
@@ -0,0 +1,16 @@
+package searching;
+
+public class SearchInSortedMatrix {
+
+    public static void main(String[] args) {
+//        strictly sorted array
+        int[][] nums = {
+                {1,2,3,5},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+                 };
+    }
+    // take middle col or row and perform Binary search on it
+
+}
